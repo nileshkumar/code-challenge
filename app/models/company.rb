20c5ebd2e0e6b98424af92e704d1d35925290010
Soft. Dev <24978+nileshkumar@users.noreply.github.com>
2020-07-21 15:46:54 +0530

    add validation for email

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,9 @@
 class Company < ApplicationRecord
   has_rich_text :description
+  
+  validates :email,
+            format: { with: /\b[A-Z0-9._%a-z\-]+@getmainstreet\.com\z/, message: 'Allowed Domain is getmainstreet.com.' },
+            allow_blank: true
 
 
 end
