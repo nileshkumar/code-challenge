46da917493656cc229dbc7f0f3dafa944c6b3d6a
Soft. Dev <24978+nileshkumar@users.noreply.github.com>
2020-07-22 06:10:34 +0530

    update city and state

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,6 +4,15 @@ class Company < ApplicationRecord
   validates :email,
             format: { with: /\b[A-Z0-9._%a-z\-]+@getmainstreet\.com\z/, message: 'Allowed Domain is getmainstreet.com.' },
             allow_blank: true
-
+  
+  before_save :get_city_state, if: Proc.new { |company| company.zip_code_changed? }
+  
+  def get_city_state
+    zip_code = ZipCodes.identify(self.zip_code.to_s)
+    if zip_code
+      self.city = zip_code[:city]
+      self.state = zip_code[:state_code]
+    end
+  end
 
 end
